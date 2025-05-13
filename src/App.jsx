615d0bca03997a5b82a8f1c04ad529ab131e9cf2
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2025-05-14 00:05:54 +0600

    add: created Header, Hero and Footer section

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Footer />
+    </>
+  );
+}
