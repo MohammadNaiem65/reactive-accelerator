a9df101842e305c6cedb84a23017dfd013833ef5
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2025-06-15 18:04:51 +0600

    add: implemented till filtering names

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,54 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createContactAction,
+  deleteContactAction,
+  editContactAction,
+} from "./actions/contactActions";
+import ErrorPage from "./ErrorPage";
+import "./index.css";
+import getContactsLoader, {
+  getContactLoader,
+} from "./loaders/getContactsLoader";
+import Contact from "./routes/Contact";
+import EditContact from "./routes/EditContact";
+import Index from "./routes/Index";
+import Root from "./routes/Root";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: getContactsLoader,
+    action: createContactAction,
+    children: [
+      {
+        index: true,
+        element: <Index />,
+      },
+      {
+        path: "/contacts/:contactId",
+        element: <Contact />,
+        loader: getContactLoader,
+      },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: getContactLoader,
+        action: editContactAction,
+      },
+      {
+        path: "contacts/:contactId/destroy",
+        action: deleteContactAction,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
--- src/utils/getData.js
+++
@@ -1,8 +0,0 @@
-export default function getData(index) {
-  const randomTime = Math.floor(Math.random() * 6) + 1;
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(`Data of request ${index + 1}`);
-    }, randomTime * 1000);
-  });
-}
